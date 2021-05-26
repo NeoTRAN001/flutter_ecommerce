9fc4bcf0f3796a7458e62b60fccfd7346e49dd91
NeoTRAN <neo_tran@outlook.com>
2021-05-26 14:24:20 -0500

    feat: add title text and image

---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/constants.dart';
+import 'package:flutter_ecommerce/size_config.dart';
+
+class Body extends StatefulWidget {
+  Body({Key key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "TOKOTO",
+                    style: TextStyle(
+                      fontSize: getProportionateScreenWidth(36),
+                      color: kPrimaryColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Welcome to Tokoto, Let's shop!"),
+                  Image.asset(
+                    "assets/images/splash_1.png",
+                    height: getProportionateScreenHeight(265),
+                    width: getProportionateScreenWidth(235),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/screens/splash/components/body.dart';
+import 'package:flutter_ecommerce/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Body(),
+    );
   }
 }
