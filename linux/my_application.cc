114feaf0d2ef3184550cebf093c696e809e5ea5b
NeoTRAN <neo_tran@outlook.com>
2021-05-26 13:54:30 -0500

    feat: config size in linux window

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -48,7 +48,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "flutter_ecommerce");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 305, 630);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
